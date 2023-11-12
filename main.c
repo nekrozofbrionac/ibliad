9c0edde65a0eeef95ce6720470dcb743ad44afb4
Chingun Ariunbat <chingun26@yahoo.de>
2023-11-13 00:25:14 +0100

    implements changing brightness outright

--- main.c
+++ main.c
@@ -27,11 +27,30 @@ int read_uint_from_file(const char *path) {
     return result;
 }
 
+int write_int_into_file(const char *path, int value) {
+    FILE *file;
+    file = fopen(path, "w");
+    if (file == NULL) {
+        return -1;
+    }
+    fprintf(file, "%d", value);
+    fclose(file);
+    return 0;
+}
+
 #define CURRENT_BRIGHTNESS_PATH "/sys/class/backlight/intel_backlight/brightness"
 #define MAX_BRIGHTNESS_PATH "/sys/class/backlight/intel_backlight/max_brightness"
 #define STEPS 20
 
+/**
+ * Reads the brightness values from intel_backlight/ and returns a new value
+ * based on how many '+' or '-' characters given to it, and relative to max_brightness
+ *
+ * Designed for Intel Backlight only.
+ */
 int main(const int argc, const char *argv[]) {
+    if (argc < 2) return 0;
+
     const int current_brightness = read_uint_from_file(CURRENT_BRIGHTNESS_PATH);
     if (current_brightness < 0) {
         return 1;
@@ -41,10 +60,6 @@ int main(const int argc, const char *argv[]) {
         return 1;
     }
 
-    if (argc < 2) {
-        printf("%d", current_brightness);
-        return 0;
-    }
 
     int step = max_brightness / STEPS;
     int output_value = current_brightness;
@@ -66,6 +81,10 @@ int main(const int argc, const char *argv[]) {
     if (output_value < 0) {
         output_value = 0;
     }
-    printf("%d", output_value);
+
+    const int result = write_int_into_file(CURRENT_BRIGHTNESS_PATH, output_value);
+    if (result < 0) {
+        return 1;
+    }
     return 0;
 }
